0975fa62c5409920d4679d8474a57f33a0ee70c4
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-04-16 14:41:54 +0000

    Add static build
    100% static allows running on systems like Alpine and Nix, I may eventually switch to Musl to reduce the size

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ cd bubblewrap
 meson -Dc_link_args="-Wl,--no-undefined /usr/lib/$ARCH-linux-gnu/libcap.a -Wl,--no-undefined /usr/lib/$ARCH-linux-gnu/libc.a" -Dbuildtype=minsize build
 cd build
 ninja
+# Why tf does it seem impossible to get static builds using Meson? Maybe I missed something but this works
+gcc -o bwrap-static bwrap.p/bubblewrap.c.o bwrap.p/bind-mount.c.o bwrap.p/network.c.o bwrap.p/utils.c.o -lcap -lc -lpthread -static -Os
 
-strip -s bwrap
+strip -s bwrap*
 mv bwrap "../../bwrap.$ARCH"
+mv bwrap-static "../../bwrap-static.$ARCH"
