5ba64969a23678a5eafb6382537fba01427c38e0
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-08-31 01:11:59 -0600

    Revert

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@
 git clone https://github.com/containers/bubblewrap
 cd bubblewrap
 
-CC='musl-gcc' meson -Dc_link_args="-Wl,--no-undefined /usr/lib/$ARCH-linux-gnu/libcap.a" -Dbuildtype=minsize build
+meson -Dc_link_args="-Wl,--no-undefined /usr/lib/$ARCH-linux-gnu/libcap.a" -Dbuildtype=minsize build
 cd build
 ninja
 # Why tf does it seem impossible to get static builds using Meson? Maybe I missed something but this works
-musl-gcc -o bwrap-static bwrap.p/bubblewrap.c.o bwrap.p/bind-mount.c.o bwrap.p/network.c.o bwrap.p/utils.c.o -lcap -lc -lpthread -static -Os
+gcc -o bwrap-static bwrap.p/bubblewrap.c.o bwrap.p/bind-mount.c.o bwrap.p/network.c.o bwrap.p/utils.c.o -lcap -lc -lpthread -static -Os
 
 strip -s bwrap*
 mv bwrap "../../bwrap.$ARCH"
