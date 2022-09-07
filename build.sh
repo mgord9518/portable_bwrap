0086677a822c67569f9f31b16e84f62c81d75fa2
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-09-06 23:20:57 -0600

    Build as library

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# The libraries I chose to statically link are based on <github.com/AppImage/pkg2appimage/blob/master/excludelist>
 
 [ -z $ARCH ] && ARCH=$(uname -m)
 
@@ -12,6 +11,17 @@ ninja
 # Why tf does it seem impossible to get static builds using Meson? Maybe I missed something but this works
 gcc -o bwrap-static bwrap.p/bubblewrap.c.o bwrap.p/bind-mount.c.o bwrap.p/network.c.o bwrap.p/utils.c.o -lcap -lc -lpthread -static -Os
 
+# Also build as a static library for use in aisap (or wherever else someone would want to)
+# bwrap must be rebuilt with the main function renamed because one executable cannot have multiple main funcs
+# Big thanks to <github.com/Seren541> for the help on how to do this
+cd ..
+sed -i 's/main /bwrap_main /g' bubblewrap.c
+meson -Dc_link_args="-Wl,--no-undefined /usr/lib/$ARCH-linux-gnu/libcap.a" -Dbuildtype=minsize --reconfigure build
+cd build
+ninja
+ar -rcs libbwrap.a bwrap.p/bubblewrap.c.o bwrap.p/bind-mount.c.o bwrap.p/network.c.o bwrap.p/utils.c.o
+
 strip -s bwrap*
 mv bwrap "../../bwrap.$ARCH"
 mv bwrap-static "../../bwrap-static.$ARCH"
+mv bwrap.a "../../bwrap-static.$ARCH.a"
